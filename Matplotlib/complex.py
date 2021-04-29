12ac8919cb25de42aa1b451a23b371e941dbd8ae
eleander <edward.leander@hotmail.com>
2021-04-29 10:46:02 +0200

    Pi chart, learnt how to import

---
+++ Matplotlib/complex.py
@@ -0,0 +1,15 @@
+class complex:
+    def __init__(self, realpart, imagpart):
+        self.r = realpart
+        self.i = imagpart
+
+    def do_something(self):
+        print("doSomething")
+
+    def do_something_else(self):
+        print("doSomethingElse")
+
+
+# Tip
+# from complex import complex
+# Python Analysis Extra Paths: matplotlib
